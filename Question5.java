567e313bebfea557577c45c235dc69ad5ee40630
Kenneth <kenwong.500@gmail.com>
2020-04-24 11:06:25 +0800

    Question 5

--- Question5.java
+++ Question5.java
@@ -1,32 +1,47 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Question5
 {
   public static void main(String[] args)
   {
-    /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
-     * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
-     * Hint: Use a loop to get input. Use another 2 loops to find the mode
-     */
-     
-    Scanner in = new Scanner(System.in);
-    
+    Scanner object = new Scanner(System.in);
+    System.out.print("How many number would you like to input?");
+    int x = object.nextInt();
+    int[] numberlist = new int[x];
+          
+    for(int i = 0; i < numberlist.length; i++) {
+      System.out.print("Input number " + (i + 1) + ":");
+       numberlist[i] = object.nextInt();
+    }
+  
+  Arrays.sort(numberlist); 
+  int n = numberlist.length;
+  
+  int countofmax = 1;
+  int temp = numberlist[0]; 
+  int count = 1; 
+  
+  for (int i = 1; i < numberlist.length; i++) 
+  { 
+    if (numberlist[i] == numberlist[i - 1]) 
+      count++; 
+    else
+    { 
+      if (count > countofmax) 
+      { 
+        countofmax = count; 
+        temp = numberlist[i - 1]; 
+      } 
+      count = 1; 
+    } 
+  } 
+
+ 
+  if (count > countofmax) 
+  { 
+    countofmax = count; 
+    temp = numberlist[n - 1]; 
+  } 
+  System.out.println("The mode is:" + temp);     
   }
 }
